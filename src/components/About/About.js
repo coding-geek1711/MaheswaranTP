9237266c50459392c7948d664c3c96dcff8c2164
coding-geek1711 <mahestpm@gmail.com>
2022-04-07 21:31:43 +0530

    MyWebsite

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <div className="flex w-screen justify-center flex-col items-center text-white bg-bluePrimary py-10">
             <h1 className="text-5xl mb-14">About</h1>
             <div className="container flex justify-evenly flex-wrap flex-row lg:flex-row">
-                <div className="image-container h-48 w-48 md:h-72 md:w-72 lg:h-96 lg:w-96 m-10 rounded-full" style={{backgroundImage:`url(${process.env.PUBLIC_URL}/image1.png)`}}></div>
+                <div className="image-container h-48 w-48 md:h-72 md:w-72 lg:h-96 lg:w-96 m-10 rounded-full" style={{backgroundImage:`url(${process.env.PUBLIC_URL}/photo.jpg)`, backgroundSize:"cover"}}></div>
                 <div className="whoami-container w-72 m-10 flex flex-col">
                     <h2 className="text-white text-3xl mb-8 text-center">Who am I?</h2>
                     <p className="text-grayPrimary text-xl">
@@ -24,6 +24,7 @@ export default function About() {
                 </div>
             </div>
             <div className="w-3/4 h-24">
+                <a href="https://drive.google.com/file/d/1CH7JUARQkmwWBOnTtp5k3mx2qTMnQKV1/view?usp=sharing"></a>
                 <ResumeButton size={"w-full h-full"}></ResumeButton>
             </div>
         </div>
--- src/components/utils/Buttons/ResumeButton.js
+++ src/components/utils/Buttons/ResumeButton.js
@@ -5,12 +5,14 @@ import './Buttons.css'
 export default function ResumeButton({size}) {
     return (
         <div>
-            <button className={"resume-button border-4 border-white text-white text-lg flex justify-center items-center "+size}>
-                <span className="m-3">Resume</span>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                </svg>
-            </button>
+            <a href={"https://docs.google.com/document/d/13ToYS1_vxGlaua8WKPGGWfWLqHm2Twhr/edit?usp=sharing&ouid=104201969490320282020&rtpof=true&sd=true"}>
+                <button className={"resume-button border-4 border-white text-white text-lg flex justify-center items-center "+size}>
+                    <span className="m-3">Resume</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                    </svg>
+                </button>
+            </a>
         </div>
     )
 }
